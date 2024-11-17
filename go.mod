9465b8410d6b82812f53a738bfee1112b5ebb6ad
Magomed Oziev <magomed.oziev2002@gmail.com>
2024-11-17 19:41:49 +0300

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oziev02/url-shortener
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hello")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	fmt.Println("Server is listening on port 8081")
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		return
+	}
+}
