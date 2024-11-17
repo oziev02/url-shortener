94a22990ac54cde766a12277652a27583baf3654
Magomed Oziev <magomed.oziev2002@gmail.com>
2024-11-17 19:49:11 +0300

    ServeMux

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
 	fmt.Println("Server is listening on port 8081")
-	err := http.ListenAndServe(":8081", nil)
+	err := server.ListenAndServe()
 	if err != nil {
 		return
 	}
