dbfdb45ae40eb64c6611c44b78687d8dfec06143
Magomed Oziev <magomed.oziev2002@gmail.com>
2024-11-17 20:05:47 +0300

    Application structure

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/oziev02/url-shortener/internal/hello"
 	"net/http"
 )
 
 func main() {
 	router := http.NewServeMux()
-	NewHelloHandler(router)
+	hello.NewHelloHandler(router)
 
 	server := http.Server{
 		Addr:    ":8081",
